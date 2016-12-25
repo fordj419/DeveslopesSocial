ac02eecee78fef114559732fa7d3198e0c385193
John Ford <johnford@johns-macbook-pro.local>
2016-12-24 23:38:49 -0500

    Initial Commit

---
+++ Devslopes Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Devslopes Social
+//
+//  Created by John Ford on 12/24/16.
+//  Copyright © 2016 John Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
